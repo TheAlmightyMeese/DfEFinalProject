c4fb18425087be999c6068c9f9927f4a48c5de17
R <r.truesdale@hotmail.co.uk>
2021-11-08 12:11:42 +0000

    Creating Main Branch

---
+++ src/main/java/com/qa/Armoury/DfEFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.Armoury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DfEFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DfEFinalProjectApplication.class, args);
+	}
+
+}
